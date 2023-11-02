278b9fa97e758f74148de15abee232e003028a88
bornav <borna.vincek1@gmail.com>
2023-11-02 21:32:42 +0100

    first commit

---
+++ configuration.nix
@@ -0,0 +1,67 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Europe/Vienna";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_AT.UTF-8";
+    LC_IDENTIFICATION = "de_AT.UTF-8";
+    LC_MEASUREMENT = "de_AT.UTF-8";
+    LC_MONETARY = "de_AT.UTF-8";
+    LC_NAME = "de_AT.UTF-8";
+    LC_NUMERIC = "de_AT.UTF-8";
+    LC_PAPER = "de_AT.UTF-8";
+    LC_TELEPHONE = "de_AT.UTF-8";
+    LC_TIME = "de_AT.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  users.users.nixos = {
+    isNormalUser = true;
+    description = "nixos";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+  vim 
+  wget
+  git
+  ];
+
+ programs.gnupg.agent = {
+   enable = true;
+   enableSSHSupport = true;
+ };
+
+
+  services.openssh.enable = true;
+  services.sshd.enable = true;
+
+  system.stateVersion = "23.11";# Did you read the comment?
+  nix = {
+	package = pkgs.nixFlakes;
+	extraOptions = "experimental-features = nix-command flakes";
+  };
+}
