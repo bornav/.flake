c3edcf46bfef032d3af7d0577793e2f5fc01ad71
bornav <borna.vincek1@gmail.com>
2023-11-18 08:45:59 +0100

    not tested

--- deployment.sh
+++ deployment.sh
@@ -1,7 +1,8 @@
+git clone https://github.com/bornav/.flake
+cd .flake
+sudo su
 mkdir -p /mnt
 mount /dev/disk/by-label/NIXOS /mnt
 mkdir -p /mnt/boot
 mount /dev/disk/by-label/BOOT /mnt/boot
-git clone https://github.com/bornav/.flake
-# cd .flake
-sudo nixos-install --flake ~/.flake#vallium
+nixos-install --flake ~/.flake#vallium
