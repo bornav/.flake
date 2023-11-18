cbeef29485ccb6138b9ea252aab463a576d9764c
bornav <borna.vincek1@gmail.com>
2023-11-18 08:52:53 +0100

    not tested

--- deployment.sh
+++ deployment.sh
@@ -5,4 +5,4 @@ mkdir -p /mnt
 mount /dev/disk/by-label/NIXOS /mnt
 mkdir -p /mnt/boot
 mount /dev/disk/by-label/BOOT /mnt/boot
-nixos-install --flake ~/.flake#vallium
+nixos-install --flake .#vallium
