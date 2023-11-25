df15298b4d9d531856f08c2da40e6183221c5e75
bornav <borna.vincek1@gmail.com>
2023-11-25 10:18:14 +0100

    ignorefile

--- deployment.sh
+++ deployment.sh
@@ -4,6 +4,7 @@ cd .flake
 sudo su
 mkdir -p /mnt;      mount /dev/disk/by-label/NIXOS /mnt
 mkdir -p /mnt/boot; mount /dev/disk/by-label/BOOT /mnt/boot
+mkdir -p /mnt/home; mount /dev/disk/by-label/home_partition /mnt/home
 nixos-install --flake .#vallium
 nixos-enter --root /mnt
 echo "root:nixos" | chpasswd
