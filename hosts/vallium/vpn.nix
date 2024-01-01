b90b339419efff11df92007ddb662cbe5ea6be33
bornav <borna.vincek1@gmail.com>
2024-01-01 16:46:24 +0100

    wireguard

--- hosts/vallium/vpn.nix
+++ hosts/vallium/vpn.nix
@@ -4,3 +4,16 @@
         zerotierone
     ];
 }
+
+# nmcli connection import type wireguard file wg0.conf to add connection to the gui
+# wg.conf example
+# [Interface]
+# Address = 10.10.1.0/24
+# DNS = 10.1.1.5
+# ListenPort = 51820
+# PrivateKey = x
+
+# [Peer]
+# AllowedIPs = 10.0.0.0/8
+# Endpoint = home.local.icylair.com:51820
+# PublicKey = x
