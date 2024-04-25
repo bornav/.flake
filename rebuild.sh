fe08d5f2d114c9e349a29ccdbefbaa79b1b87381
bornav <borna.vincek1@gmail.com>
2024-04-25 22:14:39 +0200

    asd

---
+++ rebuild.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+set -e
+pushd ~/.flake/
+pwd
+echo "changes"
+# git diff -U0 *.nix
+git diff -U0
+echo "NixOS Rebuilding ... "
+nh os switch ~/.flake -H $flake_name
+if [ $? -eq 0 ]; then
+    echo "success"
+else 
+    echo "fail" # seems to never get to it with nh
+fi
+exit
+# sudo nixos-rebuild switch &>nixos-switch.log || (cat nixos-switch.log | grep --color error && false)
+gen=$(nixos-rebuild list-generations --flake ~/.flake#$flake_name | grep current)
+git commit -am "$gen"
+popd
