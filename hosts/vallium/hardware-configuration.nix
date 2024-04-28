515ca1ddcedfaa6360b753f23fd1352b6face969
bornav <borna.vincek1@gmail.com>
2024-04-28 12:52:06 +0200

    291 current  2024-04-28 12:52:02  24.05.20240425.7bb2ccd  6.8.7                           *

--- hosts/vallium/hardware-configuration.nix
+++ hosts/vallium/hardware-configuration.nix
@@ -71,7 +71,7 @@
       nvidiaSettings = true;
       # package = config.boot.kernelPackages.nvidiaPackages.stable;
       # package = config.boot.kernelPackages.nvidiaPackages.production;
-      # package = lib.mkForce config.boot.kernelPackages.nvidiaPackages.latest;
+      package = lib.mkForce config.boot.kernelPackages.nvidiaPackages.latest;
       # package = lib.mkForce config.boot.kernelPackages.nvidiaPackages.beta;
       # forceFullCompositionPipeline = true;
       powerManagement.enable = false;
