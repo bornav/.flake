81dcb99d54d5f182201fc8ca439b59f5a0664d2f
bornav <borna.vincek1@gmail.com>
2024-05-03 20:31:20 +0200

    323 current  2024-05-03 20:31:15  24.05.20240427.58a1abd  6.8.7                           *

--- deployment.sh
+++ deployment.sh
@@ -5,6 +5,6 @@ sudo su
 mkdir -p /mnt;      mount /dev/disk/by-label/NIXOS /mnt
 mkdir -p /mnt/boot; mount /dev/disk/by-label/BOOT /mnt/boot
 mkdir -p /mnt/home; mount /dev/disk/by-label/home_partition /mnt/home
-nixos-install --flake .#vallium
+nixos-install --flake .#$1
 nixos-enter --root /mnt
 echo "root:nixos" | chpasswd
--- hosts/dockeropen/default.nix
+++ hosts/dockeropen/default.nix
@@ -1,8 +1,59 @@
 { config, pkgs, pkgs-unstable, vars, ... }:
-
 {
   imports = [
     ./hardware-configuration.nix
-    ./network-shares.nix
   ];
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  # boot.kernelPackages = pkgs-unstable.linuxKernel.packages.linux_6_8;
+  boot.loader = {
+    grub.efiSupport = true;
+    grub.enable = true;
+    grub.device = "nodev";
+    efi.canTouchEfiVariables = true;
+  };
+
+  networking.hostName = "dockeropen"; # Define your hostname.
+  networking.networkmanager.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    xkb.layout = "us";
+    xkb.variant = "";
+  };
+  users.users.${vars.user} = {
+    isNormalUser = true;
+    description = "${vars.user}";
+    extraGroups = [ "networkmanager" "wheel" "docker" "wireshark" ];
+    packages = with pkgs; [];
+  };
+  environment.sessionVariables = {
+    flake_name="dockeropen";
+    FLAKE="$HOME/.flake";
+    NIXOS_CONFIG="$HOME/.flake";
+    # NIXOS_CONFIG="/home/${vars.user}/.flake";
+    QT_STYLE_OVERRIDE="kvantum";
+    WLR_NO_HARDWARE_CURSORS = "1"; # look into removing
+    NIXOS_OZONE_WL = "1"; #Hint electron apps to use wayland
+  };
+
+  #### modules
+  virtualization.enable = true;
+  virtualisation.docker.enable = true;
+  rar.enable = true;
+
+  ####
+  nixpkgs.config.allowUnfree = true;
+  environment.systemPackages = with pkgs; [
+    jq
+    gparted
+    pciutils # lspci
+  ];
+
+  programs.nix-ld = {
+    enable = true;
+    libraries = with pkgs; [
+      # add any missing dynamic libraries for unpacked programs here, not in the environment.systemPackages
+    ];
+  };
 }
