f0efa8ee03c026e167db14a13d3a18ce1bf5abd4
bornav <borna.vincek1@gmail.com>
2024-05-10 23:52:56 +0200

    355 current  2024-05-10 23:52:53  24.05.20240509.f1010e0  6.8.9                           *

--- hosts/stealth/default.nix
+++ hosts/stealth/default.nix
@@ -123,22 +123,17 @@
   # '';
 
   nix.buildMachines = [ {
-	 hostName = "builder";
-	 system = "x86_64-linux";
-                   protocol = "ssh-ng";
-	 # if the builder supports building for multiple architectures, 
-	 # replace the previous line by, e.g.
-	 # systems = ["x86_64-linux" "aarch64-linux"];
-	 maxJobs = 4;
-	 speedFactor = 8;
-	 supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
-	 mandatoryFeatures = [ ];
-	}] ;
-	nix.distributedBuilds = true;
-	# optional, useful when the builder has a faster internet connection than yours
-	nix.extraOptions = ''
-	  builders-use-substitutes = true
-	'';
+    hostName = "builder";
+    system = "x86_64-linux";
+    protocol = "ssh-ng";
+    # if the builder supports building for multiple architectures, 
+    # replace the previous line by, e.g.
+    # systems = ["x86_64-linux" "aarch64-linux"];
+    maxJobs = 2;
+    speedFactor = 2;
+    supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+    mandatoryFeatures = [ ];
+  }] ;
   programs.ssh.extraConfig = ''
     Host builder
       HostName 10.2.11.33
--- modules/nix/builder.nix
+++ modules/nix/builder.nix
@@ -27,5 +27,10 @@ with lib;
     };
     
     nix.settings.trusted-users = [ "nixbuilder" ];
+    nix.distributedBuilds = true;
+    # optional, useful when the builder has a faster internet connection than yours
+    nix.extraOptions = ''
+      builders-use-substitutes = true
+    '';
   };
 }
