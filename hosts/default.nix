82de5a3f060747dd542fa5d2879219ca75a4ee2c
bornav <borna.vincek1@gmail.com>
2024-05-26 16:51:56 +0200

    451 current  2024-05-26 16:51:53  24.11.20240524.bfb7a88  6.9.1                           *

--- hosts/default.nix
+++ hosts/default.nix
@@ -46,57 +46,57 @@ in
           inputs.nix-flatpak.nixosModules.nix-flatpak
           ];
     };
-    # stealth = inputs.nixpkgs.lib.nixosSystem {
-    #     system = "x86_64-linux";  
-    #     specialArgs = {
-    #         inherit  pkgs-unstable vars inputs;
-    #         host = {
-    #             hostName = "stealth";
-    #         };
-    #     };
-    #     modules = [
-    #       # nur.nixosModules.nur
-    #       ./configuration.nix
-    #       ./stealth
-    #       inputs.home-manager.nixosModules.home-manager {
-    #         home-manager.useGlobalPkgs = true;
-    #         home-manager.useUserPackages = true;
-    #       }
-    #       {
-    #         nix.settings = {
-    #           substituters = [ "https://cosmic.cachix.org/" ];
-    #           trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
-    #         };
-    #       }
-    #       inputs.nixos-cosmic.nixosModules.default
-    #       inputs.nixos-hardware.nixosModules.common-pc-ssd
-    #       inputs.nixos-hardware.nixosModules.common-cpu-intel
-    #       inputs.nixos-hardware.nixosModules.common-cpu-intel-kaby-lake
-    #       ];
-    # };
-    # dockeropen = inputs.nixpkgs.lib.nixosSystem {
-    #     system = "x86_64-linux";  
-    #     specialArgs = {
-    #         inherit pkgs-unstable vars inputs;
-    #         host = {
-    #             hostName = "dockeropen";
-    #         };
-    #     };
-    #     modules = [
-    #         # nur.nixosModules.nur
-    #         ./configuration.nix
-    #         ./dockeropen
-    #         inputs.home-manager.nixosModules.home-manager {
-    #         home-manager.useGlobalPkgs = true;
-    #         home-manager.useUserPackages = true;
-    #         }
-    #         inputs.nixos-cosmic.nixosModules.default
-    #         inputs.nixos-hardware.nixosModules.common-cpu-amd
-    #         inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
-    #         inputs.nixos-hardware.nixosModules.common-gpu-nvidia-nonprime
-    #         inputs.nixos-hardware.nixosModules.common-pc-ssd
-    #         inputs.nix-flatpak.nixosModules.nix-flatpak
-    #         inputs.disko.nixosModules.disko
-    #         ];
-    # };
+    stealth = inputs.nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";  
+        specialArgs = {
+            inherit  pkgs-unstable vars inputs;
+            host = {
+                hostName = "stealth";
+            };
+        };
+        modules = [
+          # nur.nixosModules.nur
+          ./configuration.nix
+          ./stealth
+          inputs.home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+          }
+          {
+            nix.settings = {
+              substituters = [ "https://cosmic.cachix.org/" ];
+              trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+            };
+          }
+          inputs.nixos-cosmic.nixosModules.default
+          inputs.nixos-hardware.nixosModules.common-pc-ssd
+          inputs.nixos-hardware.nixosModules.common-cpu-intel
+          inputs.nixos-hardware.nixosModules.common-cpu-intel-kaby-lake
+          ];
+    };
+    dockeropen = inputs.nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";  
+        specialArgs = {
+            inherit pkgs-unstable vars inputs;
+            host = {
+                hostName = "dockeropen";
+            };
+        };
+        modules = [
+            # nur.nixosModules.nur
+            ./configuration.nix
+            ./dockeropen
+            inputs.home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            }
+            inputs.nixos-cosmic.nixosModules.default
+            inputs.nixos-hardware.nixosModules.common-cpu-amd
+            inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
+            inputs.nixos-hardware.nixosModules.common-gpu-nvidia-nonprime
+            inputs.nixos-hardware.nixosModules.common-pc-ssd
+            inputs.nix-flatpak.nixosModules.nix-flatpak
+            inputs.disko.nixosModules.disko
+            ];
+    };
 }
