581171e1781f7cd3c984caaad0cd5665a33e8e72
bornav <borna.vincek1@gmail.com>
2024-06-03 13:39:27 +0200

    485 current  2024-06-03 13:39:25  24.11.20240529.ad57eef  6.9.2                           cosmic

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,9 +1,10 @@
 { inputs, vars, ... }:
 
 let
-  # pkgs = import inputs.nixpkgs {
-  #   config.allowUnfree = true;                              # Allow Proprietary Software
-  # };
+  pkgs-stable = import inputs.nixpkgs-stable {
+    config.allowUnfree = true;                              # Allow Proprietary Software
+    system = "x86_64-linux";
+  };
 
   pkgs-unstable = import inputs.nixpkgs-unstable {
     config.allowUnfree = true;
@@ -16,61 +17,76 @@ let
   };
 in
 {
-    vallium = inputs.nixpkgs-unstable.lib.nixosSystem {
-        system = "x86_64-linux";  
-        specialArgs = {
-            # inherit (inputs.nixpkgs-unstable) lib;
-            inherit  pkgs-unstable vars inputs;
-            host = {
-                hostName = "vallium";
-            };
+  vallium = inputs.nixpkgs-unstable.lib.nixosSystem {
+    system = "x86_64-linux";  
+    specialArgs = {
+        # inherit (inputs.nixpkgs-unstable) lib;
+        inherit  vars inputs;
+        host = {
+            hostName = "vallium";
         };
-        modules = [
-          # nur.nixosModules.nur
-          ./configuration.nix
-          ./vallium
-          {
-            nix.settings = {
-              substituters = [ "https://cosmic.cachix.org/" ];
-              trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
-            };
-          }
-          ];
     };
-    stealth = inputs.nixpkgs-unstable.lib.nixosSystem {
-        system = "x86_64-linux";  
-        specialArgs = {
-            inherit  pkgs-unstable vars inputs;
-            host = {
-                hostName = "stealth";
-            };
+    modules = [
+      # nur.nixosModules.nur
+      ./configuration.nix
+      ./vallium
+      {
+        nix.settings = {
+          substituters = [ "https://cosmic.cachix.org/" ];
+          trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
         };
-        modules = [
-          # nur.nixosModules.nur
-          ./configuration.nix
-          ./stealth
-          
-          {
-            nix.settings = {
-              substituters = [ "https://cosmic.cachix.org/" ];
-              trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
-            };
-          }
-          ];
-    };
-    dockeropen = inputs.nixpkgs-unstable.lib.nixosSystem {
-        system = "x86_64-linux";  
-        specialArgs = {
-            inherit pkgs-unstable vars inputs;
-            host = {
-                hostName = "dockeropen";
-            };
+      }
+    ];
+  };
+  stealth = inputs.nixpkgs-unstable.lib.nixosSystem {
+    system = "x86_64-linux";  
+    specialArgs = {
+        inherit  pkgs-unstable vars inputs;
+        host = {
+            hostName = "stealth";
         };
-        modules = [
-            # nur.nixosModules.nur
-            ./configuration.nix
-            ./dockeropen
-            
-            ];
     };
+    modules = [
+      # nur.nixosModules.nur
+      ./configuration.nix
+      ./stealth
+      
+      {
+        nix.settings = {
+          substituters = [ "https://cosmic.cachix.org/" ];
+          trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+        };
+      }
+    ];
+  };
+  dockeropen = inputs.nixpkgs-unstable.lib.nixosSystem {
+    system = "x86_64-linux";  
+    specialArgs = {
+        inherit pkgs-unstable vars inputs;
+        host = {
+            hostName = "dockeropen";
+        };
+    };
+    modules = [
+        # nur.nixosModules.nur
+        ./configuration.nix
+        ./dockeropen
+        
+    ];
+  };
+  k3s-local = inputs.nixpkgs-stable.lib.nixosSystem {
+    system = "x86_64-linux";  
+    specialArgs = {
+        inherit pkgs-stable vars inputs;
+        host = {
+            hostName = "k3s-local";
+        };
+    };
+    modules = [
+        # nur.nixosModules.nur
+        ./configuration.nix
+        ./dockeropen
+        
+    ];
+  };
 }
