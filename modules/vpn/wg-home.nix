c63b7d17687b658a03c46ce9fcdbfe1ade9ff7aa
bornav <borna.vincek1@gmail.com>
2025-04-27 00:16:16 +0200

    14 current  2025-04-26 23:52:56  25.05.20250424.f771eb4  6.12.24                          *

--- modules/vpn/wg-home.nix
+++ modules/vpn/wg-home.nix
@@ -1,46 +1,50 @@
 { config, system, lib, ... }:
 with lib;
 {   
-    options = {
-      wg-home = {
-        enable = mkOption {
-          type = types.bool;
-          default = false;
-        };
-        local_ip = mkOption {
-          type = types.string;
-          default = "10.10.1.0/24";
-        };
+  options = {
+    wg-home = {
+      enable = mkOption {
+        type = types.bool;
+        default = false;
+      };
+      local_ip = mkOption {
+        type = types.string;
+        default = "10.10.1.0/24";
       };
     };
-    config = mkIf (config.wg-home.enable) {
-      networking.wg-quick.interfaces = {
-        wg9 = {
-          address = [ config.wg-home.local_ip ];
-          dns = [ "10.1.1.1" ];
-          privateKeyFile = "/home/bocmo/.ssh/wg/priv.key";
-          
-          peers = [
-            {
-              publicKey = "ijU4YxKoxbBwGZBIxuo8SXYtd9mU3Fug77ZdpM+0OUo=";
-              allowedIPs = [ "10.0.0.0/8"];
-              endpoint = "home.local.icylair.com:51820";
-              persistentKeepalive = 25;
-            }
-          ];
-        };
+  };
+  config = mkIf (config.wg-home.enable) {
+    networking.wg-quick.interfaces = {
+      wg9 = {
+        address = [ config.wg-home.local_ip ];
+        dns = [ "10.10.1.1" ];
+        privateKeyFile = "/home/bocmo/.ssh/wg/priv.key";
+        peers = [
+          {
+            publicKey = "ijU4YxKoxbBwGZBIxuo8SXYtd9mU3Fug77ZdpM+0OUo=";
+            allowedIPs = [ "10.0.0.0/8"];
+            endpoint = "home.local.icylair.com:51820";
+            persistentKeepalive = 25;
+          }
+        ];
       };
+      
     };
+  #   networking.wireguard.enable = true;
+  #   networking.wireguard.interfaces = {
+  #     wg0 = {
+  #       ips = [ config.wg-home.local_ip ];
+  #       listenPort = 51820;
+  #       privateKeyFile = "/home/bocmo/.ssh/wg/priv.key";
+  #       peers = [
+  #         {
+  #           publicKey = "ijU4YxKoxbBwGZBIxuo8SXYtd9mU3Fug77ZdpM+0OUo=";
+  #           allowedIPs = [ "10.0.0.0/8"];
+  #           endpoint = "home.local.icylair.com:51820";
+  #           persistentKeepalive = 25;
+  #         }
+  #       ];
+  #     };
+  #   };
+  };
 }
-# nmcli connection import type wireguard file wg0.conf to add connection to the gui
-# wg.conf example
-# [Interface]
-# Address = 10.10.1.0/24
-# DNS = 10.1.1.5
-# ListenPort = 51820
-# PrivateKey = x
-
-# [Peer]
-# AllowedIPs = 10.0.0.0/8
-# Endpoint = home.local.icylair.com:51820
-# PublicKey = x
