37b8030a4449f698ee1c25efc3520cf50fa021cc
bornav <borna.vincek1@gmail.com>
2025-05-21 20:27:53 +0200

    1131 current  2025-05-21 20:27:31  25.11.20250520.2795c50  6.14.7                          gnome

--- modules/custom_pkg/install_thorium.nix
+++ modules/custom_pkg/install_thorium.nix
@@ -9,14 +9,17 @@ with lib;
 let
     # note to check if valid, make sure otherl like middleclickscroll work, on one being bad all features will stop working
     # https://github.com/Alex313031/thorium/blob/9bdeac89dd5cebe7120c063942ce0842dea40dec/infra/CMDLINE_FLAGS_LIST.md?plain=1#L1054 location of all flags
-    thorium-flags_dot_conf = ''
-        --enable-blink-features=MiddleClickAutoscroll 
-        --enable-features=VaapiOnNvidiaGPUs
-        --gtk-version=4 
-        --custom-ntp=https://dashboard.icylair.com
-        --enable-features=UseOzonePlatform 
-        --ozone-platform=wayland
-    '';
+    ## seems this stopped working, investigate why
+    # thorium-flags_dot_conf = ''
+    #     --enable-blink-features=MiddleClickAutoscroll 
+    #     --enable-features=VaapiOnNvidiaGPUs
+    #     --gtk-version=4 
+    #     --custom-ntp=https://dashboard.icylair.com
+    #     --enable-features=UseOzonePlatform 
+    #     --ozone-platform-hint=auto 
+    # '';
+        # --ozone-platform=wayland
+    thorium-flags_dot_conf = ''--enable-blink-features=MiddleClickAutoscroll --enable-features=VaapiOnNvidiaGPUs --gtk-version=4 --custom-ntp=https://dashboard.icylair.com --enable-features=UseOzonePlatform --ozone-platform-hint=auto'';
 in
 {
   options = {
