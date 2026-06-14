46fce594248822806ebedefb42f151f6a33830b5
bornav <borna.vincek1@gmail.com>
2026-06-14 19:06:05 +0200

    1681        2026-06-14 19:05:46  26.11.20260610.9ae611a  7.0.12  Unknown                 []              True

--- hosts/dockeropen/alloy.nix
+++ hosts/dockeropen/alloy.nix
@@ -7,8 +7,7 @@
   # docker_socket = "unix:///var/run/podman/podman.sock";
   prometheus_ingest = "http://prometheus.internal:9090/api/v1/write";
   loki_ingest = "http://loki.internal:3100/loki/api/v1/push";
-  otel_ingest = "otel-collector.internal:4318";
-  # otel_ingest = "10.2.11.42:3418";
+  otel_ingest = "tempo.internal:4317";
   pytoscope_ingest = "http://pyroscope.internal:4100";
 in {
   ####################### all required in order for alloy to have perm access to docker/podman socket
@@ -20,65 +19,63 @@ in {
   services.alloy.enable = true;
   services.alloy.extraFlags = ["--disable-reporting"]; # this removes the anon usage statistics
   environment.etc."alloy/config.alloy".text = lib.mkForce ''
-    otelcol.exporter.otlphttp "local" {
+    livedebugging {
+      enabled = true
+    }
+    prometheus.remote_write "local" {
+      endpoint {
+        url = "${prometheus_ingest}"
+      }
+    }
+    loki.write "local" {
+      endpoint {
+        url = "${loki_ingest}"
+      }
+    }
+    otelcol.exporter.otlp "local" {
       client {
-        endpoint = "http://${otel_ingest}"
-        timeout = "30s"
-        tls {
-          insecure = true
-        }
+        endpoint = "${otel_ingest}"
       }
     }
-    otelcol.processor.batch "default" {
-      timeout = "10s"
-      send_batch_size = 100
-      send_batch_max_size = 200
-      output {
-        logs = [otelcol.exporter.otlphttp.local.input]
-        metrics = [otelcol.exporter.otlphttp.local.input]
-        traces = [otelcol.exporter.otlphttp.local.input]
+    pyroscope.write "local" {
+      endpoint {
+        url = "${pytoscope_ingest}"
       }
     }
-    otelcol.receiver.loki "default" {
-      output {
-        logs = [otelcol.processor.batch.default.input]
-      }
-    }
-    otelcol.receiver.prometheus "default" {
-      output {
-        metrics = [otelcol.processor.batch.default.input]
-      }
-    }
-    prometheus.exporter.unix "node" {}
     prometheus.scrape "linux_node" {
       targets = prometheus.exporter.unix.node.targets
-      forward_to = [otelcol.receiver.prometheus.default.receiver]
+      forward_to = [
+        prometheus.remote_write.local.receiver,
+      ]
     }
+    prometheus.exporter.unix "node" {}
     loki.relabel "journal" {
       forward_to = []
       rule {
         source_labels = ["__journal__systemd_unit"]
-        target_label = "unit"
+        target_label  = "unit"
       }
       rule {
         source_labels = ["__journal__boot_id"]
-        target_label = "boot_id"
+        target_label  = "boot_id"
       }
       rule {
         source_labels = ["__journal__transport"]
-        target_label = "transport"
+        target_label  = "transport"
       }
       rule {
         source_labels = ["__journal_priority_keyword"]
-        target_label = "level"
+        target_label  = "level"
       }
       rule {
         source_labels = ["__journal__hostname"]
-        target_label = "instance"
+        target_label  = "instance"
       }
     }
     loki.source.journal "read" {
-      forward_to = [otelcol.receiver.loki.default.receiver]
+      forward_to = [
+        loki.write.local.receiver,
+      ]
       relabel_rules = loki.relabel.journal.rules
       labels = {
         "job" = "integrations/node_exporter",
@@ -88,7 +85,7 @@ in {
       host = "${docker_socket}"
     }
     discovery.relabel "logs_integrations_docker" {
-        targets = discovery.docker.linux.targets
+        targets = []
         rule {
             target_label = "job"
             replacement  = "integrations/docker"
@@ -109,38 +106,40 @@ in {
     }
     loki.source.docker "default" {
       host = "${docker_socket}"
-      targets = discovery.relabel.logs_integrations_docker.output
+      targets = discovery.docker.linux.targets
       relabel_rules = discovery.relabel.logs_integrations_docker.rules
       labels = {}
-      forward_to = [otelcol.receiver.loki.default.receiver]
+      forward_to = [
+        loki.write.local.receiver,
+      ]
     }
   '';
 
-  systemd.services.beyla = {
-    after = [ "network.target"];
-    # environment = { BEYLA_NETWORK_PRINT_FLOWS = "true";};
-    serviceConfig = {
-      ExecStart = "${pkgs-local.beyla}/bin/beyla -config /etc/beyla/config.yaml";
-      Restart = "on-failure";
-      RestartSec = "5s";
-      RemainAfterExit = true;
-    };
-    wantedBy = [ "multi-user.target" ];
-  };
-  environment.systemPackages = [pkgs-local.beyla];
-  environment.etc."beyla/config.yaml".text = lib.mkForce ''
-    network:
-      enable: true
-    # log_level: DEBUG
-    discovery:
-      instrument:
-        - open_ports: 80-9999
-    otel_metrics_export:
-      endpoint: http://localhost:4318
-    otel_traces_export:
-      endpoint: http://localhost:4318
-    ebpf:
-      context_propagation: all
-      track_request_headers: true
-  '';
+  # systemd.services.beyla = {
+  #   after = [ "network.target"];
+  #   # environment = { BEYLA_NETWORK_PRINT_FLOWS = "true";};
+  #   serviceConfig = {
+  #     ExecStart = "${pkgs-local.beyla}/bin/beyla -config /etc/beyla/config.yaml";
+  #     Restart = "on-failure";
+  #     RestartSec = "5s";
+  #     RemainAfterExit = true;
+  #   };
+  #   wantedBy = [ "multi-user.target" ];
+  # };
+  # environment.systemPackages = [pkgs-local.beyla];
+  # environment.etc."beyla/config.yaml".text = lib.mkForce ''
+  #   network:
+  #     enable: true
+  #   # log_level: DEBUG
+  #   discovery:
+  #     instrument:
+  #       - open_ports: 80-9999
+  #   otel_metrics_export:
+  #     endpoint: http://localhost:4318
+  #   otel_traces_export:
+  #     endpoint: http://localhost:4318
+  #   ebpf:
+  #     context_propagation: all
+  #     track_request_headers: true
+  # '';
 }
